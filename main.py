473d116d6911bcc5a4ba378d75c8cbf49d3871c0
dotslash227 <dhruv@dhruvarora.in>
2022-04-15 17:26:16 -0400

    added headless and sandbox version

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ def login_to_url(driver):
 
 def usa_visa_checker():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     ua = UserAgent(verify_ssl=False)
     user_agent = ua.random
     chrome_options.add_argument(f"user-agent={user_agent}")
